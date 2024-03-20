59d89e7715304a70d0d86c7f2381756cb609941d
4K416 <devhunter.4k0416@outlook.com>
2024-03-21 03:20:52 +0900

    front 4 commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { NgOptimizedImage, CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NgOptimizedImage, CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'frontend';
+  title = "frontend";
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
-
+  imageUrl = "../assets/images/Loop2Earn_big.png"
 }
